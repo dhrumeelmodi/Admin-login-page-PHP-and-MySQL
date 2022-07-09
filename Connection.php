026fc9b9726a7c70161336427f4607667e04ddbe
Dhrumil Modi <71444593+dhrumeelmodi@users.noreply.github.com>
2022-07-09 17:01:53 +0530

    Create Connection.php

---
+++ Connection.php
@@ -0,0 +1,13 @@
+<?php 
+
+$con=mysqli_connect("localhost","root","","admin_database");
+
+if(mysqli_connect_error()){
+    echo"Cannot connect to database";
+}
+// else{
+//     echo"Connection established to database successfully";
+// }
+
+
+?>
